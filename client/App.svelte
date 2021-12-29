95c2b566e75a28a78661a9e44ca92b0925482976
Sam VanTassel <vantassel.sam@gmail.com>
2021-12-28 17:18:14 -0800

    feat: minimal frontend test app

---
+++ client/App.svelte
@@ -0,0 +1,21 @@
+<script>
+  let mph = 30;
+  let excitement = '';
+  
+  $: speedBanner = mph.repeat(mph);
+
+  function timeTravel() {
+    if (!mph) mph = 88;
+    excitement += '!';
+  }
+</script>
+
+<h1> Approaching {mph} miles per hour{excitement}</h1>
+<button on:click={timeTravel}>Time travel!</button>
+<p>{speedBanner}</p>
+
+<style>
+  h1 {
+    color: orangered;
+  }
+</style>
