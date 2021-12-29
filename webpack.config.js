cac29c7f055a3f67f1e6763f58c6728c19aef433
Trevor Leung <trevorleung@trevorleungsmbp.lan>
2021-12-28 17:13:51 -0800

    updates with npm packages

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,4 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 const path = require('path');
 
 module.exports = {
@@ -7,10 +6,9 @@ module.exports = {
     entry: './client/index.js',
     output: {
         path: path.resolve('build'),
-        // publicPath: '/build/',
         filename: 'bundle.js',
     },
-    plugins: [new MiniCssExtractPlugin(), new HtmlWebpackPlugin({template: './index.html'})],
+    plugins: [new HtmlWebpackPlugin({template: './index.html'})],
     module: {
         rules: [
             {
@@ -18,30 +16,9 @@ module.exports = {
                 exclude: /node_modules/,
                 use: ['babel-loader'],
             },
-            {
-                test: /\.(png|jpg|gif)$/i,
-                use:[{
-                        loader: 'url-loader',
-                        options: {
-                        limit: 8192,
-                        },
-                }]
-            },
-            {
-                test: /\.s[ac]ss$/i,
-                use: [ 
-                    MiniCssExtractPlugin.loader,
-                    // "style-loader", // this is only used if we don't use the css plugin
-                    "css-loader",
-                    "sass-loader",
-                ]
-            },
         ],
     },
     devServer: {
-        // static: {
-        //     directory: path.resolve('./'),
-        // },
         compress: true,
         port: 8080,
         proxy: {
