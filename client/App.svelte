e9fee4e42c705de1ab27f6159ccdb036166b2265
Sam VanTassel <vantassel.sam@gmail.com>
2021-12-29 17:42:33 -0800

    feat: testing ap functional with lots og console logs tied to event listeners

--- client/App.svelte
+++ client/App.svelte
@@ -1,22 +1,51 @@
 <script lang="ts">
   let mph: number = 30;
   let excitement :string = '';
+  log();
   
-  $: speedBanner = `${mph} `.repeat(mph);
+  // $: speedBanner = `${mph} `.repeat(mph);
 
   function timeTravel() :void {
+    addChild();
     if (mph * 1.1 < 88) mph = Math.floor(mph * 1.1);
     else {
       mph = 88
       excitement += '!';
     }
   }
+  let parent;
+  let things = ['toy', 'calculator', 'ice crean']
+  function addChild() {
+    // const child = document.createElement('button');
+		// parent.appendChild(child);
+    things = [...things, 'another thing'];
+  }
+  // TEST FUNCTION TO LOG EVENTS
+  function log() {
+  // window.document.addEventListener('SvelteRegisterBlock', e => svelteVersion = e.detail.version, { once: true })
+  window.document.addEventListener('SvelteRegisterComponent', (e) => console.log('SvelteRegisterComponent', e))
+  window.document.addEventListener('SvelteRegisterBlock', (e) => console.log('SvelteRegisterBlock', e))
+  window.document.addEventListener('SvelteDOMInsert', (e) => console.log('SvelteDOMInsert', e))
+  window.document.addEventListener('SvelteDOMRemove',(e) => console.log('SvelteDOMRemove', e))
+  window.document.addEventListener('SvelteDOMAddEventListener', (e) => console.log('SvelteDOMAddEventListener', e))
+  window.document.addEventListener('SvelteDOMRemoveEventListener', (e) => console.log('SvelteDOMRemoveEventListener', e))
+  window.document.addEventListener('SvelteDOMSetData', (e) => console.log('SvelteDOMSetData', e))
+  window.document.addEventListener('SvelteDOMSetProperty', (e) => console.log('SvelteDOMSetProperty', e))
+  window.document.addEventListener('SvelteDOMSetAttribute', (e) => console.log('SvelteDOMSetAttribute', e))
+  window.document.addEventListener('SvelteDOMRemoveAttribute', (e) => console.log('SvelteDOMRemoveAttribute', e))
+  }
 </script>
 <div class="container">
   <h1> {mph < 88 ? "Cruising at" : "Approaching"} {mph} miles per hour{excitement}</h1>
   <button on:click={timeTravel}>Time travel!</button>
-  <p class="banner">{speedBanner}</p>
+  <!-- <p class="banner">{speedBanner}</p> -->
+  <div bind:this={parent}></div>
 </div>
+<ul>
+  {#each things as thing}
+    <li>{thing}</li>
+  {/each}
+</ul>
 
 <style>
   h1 {
--- rollup.config.js
+++ rollup.config.js
@@ -40,7 +40,7 @@ export default {
 	},
 	plugins: [
 		svelte({
-			preprocess: sveltePreprocess({ sourceMap: !production }),
+			preprocess: sveltePreprocess({ sourceMap: true }),
 			compilerOptions: {
 				// enable run-time checks when not in production
 				dev: !production
@@ -61,7 +61,7 @@ export default {
 		}),
 		commonjs(),
 		typescript({
-			sourceMap: !production,
+			sourceMap: true,
 			inlineSources: !production
 		}),
 
