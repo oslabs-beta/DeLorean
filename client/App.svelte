46e02aad3f1b6d55dbb42642b4501fccd7b17c10
Sam VanTassel <vantassel.sam@gmail.com>
2021-12-29 16:26:22 -0800

    build: converted from webpack to rollup. built basic svelte app. expect issues integrating with server build.

--- client/App.svelte
+++ client/App.svelte
@@ -1,21 +1,38 @@
-<script>
-  let mph = 30;
-  let excitement = '';
+<script lang="ts">
+  let mph: string = "30";
+  let excitement :string = '';
   
-  $: speedBanner = mph.repeat(mph);
+  $: speedBanner = mph.repeat(Number(mph));
 
-  function timeTravel() {
-    if (!mph) mph = 88;
-    excitement += '!';
+  function timeTravel() :void {
+    const mphNum: number = Number(mph)
+    if (mphNum * 1.1 < 88) mph = Math.floor(mphNum * 1.1).toString();
+    else {
+      (mph = "88");
+      excitement += '!';
+    }
   }
 </script>
-
-<h1> Approaching {mph} miles per hour{excitement}</h1>
-<button on:click={timeTravel}>Time travel!</button>
-<p>{speedBanner}</p>
+<div class="container">
+  <h1> Approaching {mph} miles per hour{excitement}</h1>
+  <button on:click={timeTravel}>Time travel!</button>
+  <p class="banner">{speedBanner}</p>
+</div>
 
 <style>
+  .container {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    font-family: Arial, Helvetica, sans-serif;
+    background-color: blanchedalmond;
+    box-shadow: rgb(156, 145, 127) 5px 5px 5px;
+  }
   h1 {
     color: orangered;
+  }
+  .banner {
+    color: navy;
+    margin: 5em;
   }
 </style>
