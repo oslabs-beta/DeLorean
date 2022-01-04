cef9004cb74e62c982d499b0c510901fccf9e1fb
Sam VanTassel <vantassel.sam@gmail.com>
2022-01-03 20:29:31 -0800

    feat: port connected between service worker and context script, registering DOM events in service worker

--- chrome_extension/background.js
+++ chrome_extension/background.js
@@ -1,9 +1,41 @@
 // This is a service worker. Its console logs will appear in a service worker console
 console.log('console log from global in background.js')
 
-chrome.runtime.onMessage.addListener((request, sender) => {
-  console.log(sender.tab ? "from a content script: " + sender.tab.url : "from the extension");
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+  console.log(`message received ${sender.tab ? "from a content script: " + sender.tab.url : "from the extension"}`);
   if (request.greeting === "hello") {
-    console.log('received hello from content script')
+    sendResponse({farewell: 'goodbye'});
+    console.log('received hello from content script', request);
   }
+  if (request.body === 'openPort') {
+    console.log('attempting to open port from background.js');
+    sendResponse({body: 'trying to open port'});
+    openPort();
+  }
+  console.log(request);
+  return true; // this line is needed to set sendReponse to be asynchronous
 })
+
+let contentScriptPortOpen = false;
+async function openPort() {
+  // Query tab
+  let queryOptions = { active: true, currentWindow: true };
+  let tabs = await chrome.tabs.query(queryOptions);
+  console.log('tabs', tabs);
+
+  // Open up connection
+  const port = chrome.tabs.connect(tabs[0].id, {
+      name: "contentScript",
+  });
+  contentScriptPortOpen = true;
+  port.postMessage({
+      body: 'test message from background to contentsript over port'
+  });
+
+  port.onMessage.addListener(function(msg) {
+      if (msg) {
+      console.log('this is msg from port (content script): ', msg);
+      }
+  })
+}
+
--- chrome_extension/main.js
+++ chrome_extension/main.js
@@ -1,1 +1,8 @@
+const connectButton = window.document.getElementById('connectButton')
+connectButton.addEventListener('click', () => {
+  console.log('attempting to send message to service worker')
+  chrome.runtime.sendMessage({body: 'openPort'}, (response) => {
+    console.log('in the response callback in main.js, got this back: ' + response.body)
+  });
+})
 
