2752a0ed57e52b2096adf2efd0fc683da6de729f
Albert Han <AlbertHan@MacBook-Pro.local>
2022-01-07 11:46:53 -0800

    refactored App.svelte to incorporate svelte logic rather than vanilla DOM manipulation

--- chrome_extension/src/State.svelte
+++ chrome_extension/src/State.svelte
@@ -3,16 +3,24 @@
 </script>
 
 <div>
-  <slot></slot>
   <ul>
-  {#each compState as state}
-  <li>{state}</li>   
-  {/each}
+    {#each compState || [] as state, i}
+      {#if state}
+        {#if Array.isArray(state)}
+          <li>Array: 
+          <svelte:self compState={state} />
+          </li>
+        {:else}
+          <li>{state}</li>
+        {/if}
+      {/if}
+    {/each}
   </ul>
 </div>
 
 <style>
   li {
-    color: blue;
+    color: rgb(0, 107, 194);
+    font-family: monospace;
   }
 </style>
--- rollup.config.js
+++ rollup.config.js
@@ -190,5 +190,5 @@ export default [
     watch: {
       clearScreen: false,
     },
-  }
+  },
 ];
