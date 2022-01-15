3e8fc0b1c521e0d5a913ada9371279e56e49cb69
Albert Han <AlbertHan@MacBook-Pro.local>
2022-01-14 18:51:59 -0800

    minor styling changes to panel

--- chrome_extension/src/App.svelte
+++ chrome_extension/src/App.svelte
@@ -6,10 +6,11 @@
   let activeIndex = 0;
   $: compState = snapshot[activeIndex];
 
-  const connectButton = window.document.getElementById("connectButton");
+
   let mainToBgPort;
-  connectButton.addEventListener("click", () => {
-    // sending message from chrome devtool extension to background.js to signify open port connection between two files
+
+  //handle click for connect button
+  function connect() {
     chrome.runtime.sendMessage({ body: "runContentScript" }, (response) => {
       // chrome.runtime.sendMessage({ body: "openPort" }, (response) => {});
     });
@@ -23,15 +24,14 @@
     });
     mainToBgPort.postMessage({ body: "testing port from main to bg" });
     connectButton.style.visibility = "hidden";
-  });
+  }
 
-  // experimental update of script
-  const secondButton = window.document.getElementById("secondButton");
-  secondButton.addEventListener("click", () => {
+  //handle click to second button
+  function secondButtonClick(){
     mainToBgPort.postMessage({ body: "updateScript", script: bundleResource });
     secondButton.style.visibility = "hidden";
-  })
-  // experimental time travel to reset to stx at index i
+  }
+
   const sendCtxIndex = (i) => {
     mainToBgPort.postMessage({ body: "updateCtx", ctxIndex: i });
   }
@@ -49,7 +49,12 @@
       }
   }});
   </script>
-
+<div>
+  <span>
+    <button id="connectButton" on:click={connect}>Connect</button>
+    <button id="secondButton" on:click={secondButtonClick}>Click me second</button>
+  </span>
+</div>
 <div id="main-page">
   <div class="buttons">
     {#each snapshot as instance, i}
@@ -66,6 +71,17 @@
 </div>
 
 <style>
+  #connectButton, #secondButton{
+    background-color: white;
+    border-radius: 5px;
+    padding: 5px;
+  }
+
+  #connectButton:hover, #secondButton:hover{
+    background-color: rgb(230, 230, 230);
+    transition-duration: .2s;
+  }
+
   #main-page {
     display: flex;
   }
@@ -75,12 +91,15 @@
     flex-direction: column;
     border-right: 1px grey solid;
     padding-right: 10px;
+    max-height: 100vh;
+    overflow: scroll;
+    width: 125px;
   }
   .stateButton {
     background-color: rgb(230, 230, 230);
     border: 1px solid white;
     border-radius: 5px;
-    width: 120px;
+    width: 100%;
     height: 40px;
     font-family: monospace;
     font-size: 12px;
