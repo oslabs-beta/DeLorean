faf586f4b51c3b7491fc467459064c69fa6318b6
Sam VanTassel <vantassel.sam@gmail.com>
2022-01-17 15:46:02 -0800

    fix: convert .ts files to .js

--- chrome_extension/panel.ts
+++
@@ -1,8 +0,0 @@
-//creates a panel within Chrome DevTools named DeLorean
-
-chrome.devtools.panels.create(
-  'DeLorean', // title
-  './SvelteDeLorean.png', // icon
-  './panel.html',
-  (panel) => {}
-);
