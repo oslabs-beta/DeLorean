9e92ab184ee68a2b91388b13e8a3b866853c3411
Aram Krakirian <aramkrakirian@gmail.com>
2022-01-19 10:26:41 -0800

    building out testing

--- testing/test.js
+++ testing/test.js
@@ -1,4 +1,5 @@
 import { render, fireEvent } from '@testing-library/svelte';
+import { screen } from '@testing-library/dom';
 import { chrome } from 'jest-chrome';
 import App from '../chrome_extension/src/App.svelte';
 
@@ -7,11 +8,21 @@ import App from '../chrome_extension/src/App.svelte';
  */
 
 describe('rendering of main App component', () => {
-  const results = render(App);
+  let results;
+
+  beforeEach(() => {
+    results = render(App);
+  });
 
   it('only displays the connect button', () => {
-    expect(() => results.getByText('Connect')).not.toThrow();
+    expect(() => results.getByText('Connect').not.toThrow());
     expect(() => results.getAllByRole('button').toHaveLength(1));
+  });
+
+  it('hides the connect button once clicked and display a single state button', async () => {
+    expect(() => results.getByText('Connect').not.toThrow());
+    await fireEvent.click(results.getByText('Connect'));
+    expect(() => results.getByText('Connect').toThrow());
   });
 });
 
